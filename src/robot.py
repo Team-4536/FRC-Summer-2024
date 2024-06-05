5b32d5a763d76ebf7fdb53875eaacdb5a80e634b
Michael-Foley <mfoley001@stpaul.k12.mn.us>
2024-06-05 09:40:34 -0500

    test format

--- src/robot.py
+++ src/robot.py
@@ -56,7 +56,6 @@ class Robot(wpilib.TimedRobot):
         self.driveGyroYawOffset = (
             0.0  # the last angle that drivers reset the field oriented drive to zero at
         )
-
         self.autoSideChooser = wpilib.SendableChooser()
         wpilib.SmartDashboard.putData("auto side chooser", self.autoSideChooser)
 
