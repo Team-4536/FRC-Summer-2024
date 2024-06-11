b65906be1d07293ff79efea0486bcd179a38f736
rob <robbiemaldrich@gmail.com>
2024-06-11 16:54:34 -0500

    all motors in the build outline and their encoders. This will need to be changed in the future

--- src/robotHAL.py
+++ src/robotHAL.py
@@ -27,7 +27,61 @@ class RobotHALBuffer:
 
 class RobotHAL:
     def __init__(self) -> None:
-        self.prev = RobotHALBuffer()
+        self.prev: RobotHALBuffer = RobotHALBuffer()
+
+        self.leftDriveMotors: list[rev.CANSparkMax] = [
+            rev.CANSparkMax(0, rev.CANSparkMax.MotorType.kBrushless),
+            rev.CANSparkMax(1, rev.CANSparkMax.MotorType.kBrushless),
+        ]
+        self.rightDriveMotors: list[rev.CANSparkMax] = [
+            rev.CANSparkMax(2, rev.CANSparkMax.MotorType.kBrushless),
+            rev.CANSparkMax(3, rev.CANSparkMax.MotorType.kBrushless),
+        ]
+
+        self.leftDriveEncoders: list[rev.SparkRelativeEncoder] = [
+            x.getEncoder() for x in self.leftDriveMotors
+        ]
+        self.rightDriveEncoders: list[rev.SparkRelativeEncoder] = [
+            x.getEncoder() for x in self.rightDriveMotors
+        ]
+
+        self.intakePivot: rev.CANSparkMax = rev.CANSparkMax(
+            4, rev.CANSparkMax.MotorType.kBrushless
+        )
+        self.intakePivotEncoder: rev.SparkRelativeEncoder = (
+            self.intakePivot.getEncoder()
+        )
+
+        self.intakeFeed: rev.CANSparkMax = rev.CANSparkMax(
+            5, rev.CANSparkMax.MotorType.kBrushless
+        )
+        self.intakeFeedEncoder: rev.SparkRelativeEncoder = self.intakeFeed.getEncoder()
+
+        self.shooterFeed: rev.CANSparkMax = rev.CANSparkMax(
+            6, rev.CANSparkMax.MotorType.kBrushless
+        )
+        self.shooterFeedEncoder: rev.SparkRelativeEncoder = (
+            self.shooterFeed.getEncoder()
+        )
+
+        self.shooterAim: rev.CANSparkMax = rev.CANSparkMax(
+            7, rev.CANSparkMax.MotorType.kBrushless
+        )
+        self.shooterAimEncoder: rev.SparkRelativeEncoder = self.shooterAim.getEncoder()
+
+        self.shooterTopMotor: rev.CANSparkMax = rev.CANSparkMax(
+            8, rev.CANSparkMax.MotorType.kBrushless
+        )
+        self.shooterTopMotorEncoder: rev.SparkRelativeEncoder = (
+            self.shooterTopMotor.getEncoder()
+        )
+
+        self.shooterBottomMotor: rev.CANSparkMax = rev.CANSparkMax(
+            9, rev.CANSparkMax.MotorType.kBrushless
+        )
+        self.shooterBottomMotorEncoder: rev.SparkRelativeEncoder = (
+            self.shooterBottomMotor.getEncoder()
+        )
 
     def update(self, buf: RobotHALBuffer, time: TimeData) -> None:
         prev = self.prev
